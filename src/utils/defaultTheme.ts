e138a6c3f43ef68f6474a2d2ae36b1362b7c8667
Robin Kehl <robin.kehl@singular-it.de>
2022-07-13 14:27:18 +0200

    feat: add styling with prop `classes`

--- src/utils/defaultTheme.ts
+++ src/utils/defaultTheme.ts
@@ -2,7 +2,7 @@ import type {InputClasses} from '../interface'
 
 const defaultTheme: InputClasses = {
   wrapper: 'shadow appearance-none border rounded px-3',
-  input: 'text-gray-700 leading-tight focus:outline-none h-64',
+  input: 'text-gray-700 leading-tight focus:outline-none',
 }
 
 export default defaultTheme
