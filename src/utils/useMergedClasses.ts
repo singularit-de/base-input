22c8632bd59fcdfd919b29a3d6755737636c6982
Robin Kehl <robin.kehl@singular-it.de>
2022-07-18 17:05:07 +0200

    refactor: `InputClasses` -> `InputClass`

--- src/utils/useMergedClasses.ts
+++ src/utils/useMergedClasses.ts
@@ -1,9 +1,9 @@
 import type {ComputedRef} from 'vue'
 import {computed} from 'vue'
-import type {InputClasses} from '../interface'
+import type {InputClass} from '../interface'
 import defaultTheme from './defaultTheme'
 
-export default function useMergedClassesRef(classes: Partial<InputClasses>): ComputedRef<InputClasses> {
+export default function useMergedClassesRef(classes: Partial<InputClass>): ComputedRef<InputClasses> {
   return computed(() => {
     return {
       ...defaultTheme,
